0b776e87cd7d3b5a22c8de6dd3d34f43e7ba022d
Adam <acdoyle630@gmail.com>
2017-03-15 16:57:22 -1000

    setUP'

--- foreclosure.js
+++ foreclosure.js
@@ -1,6 +1,37 @@
 'use strict';
-
+	
 var steve;
 var stevesLoan;
-var month =0;
+var month = 0;
 var monthsUntilEvicted;
+
+function loan () {
+	var account = {
+		borrowed: 550000,
+		balance:286000,
+		monthlyPayment: 1700,
+		defaulted: 0,
+		defaultsToForclose: 5,
+		forclosed: false 
+	};
+	function missPayment() {
+		account.defaulted ++;
+		if(account.defaulted >= account.defaultsToForclose) {
+			account.forclosed = true;
+
+		}
+		return {
+			getBalance: function (){
+				return account.balance;
+			},
+			recievePayment: function(amount){
+				if(amount < account.monthlyPayment) {
+					missPayment();
+				}
+				account.balance-=amount;
+			}
+
+		};
+
+	}
+  }
